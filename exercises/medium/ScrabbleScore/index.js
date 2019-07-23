e99fe6272f87cc7e44891681256eddb9ab5ba6ec
MartaBumane <marta.svetina@gmail.com>
2019-07-23 18:56:43 +0300

    deleted empty exercises

--- exercises/medium/ScrabbleScore/index.js
+++ exercises/medium/ScrabbleScore/index.js
@@ -37,3 +37,64 @@
  *      You can play a double or a triple letter.
  *      You can play a double or a triple word.
  */
+function score(inputString){
+    inputString= inputString.toUpperCase();
+    let score = 0;
+    let resultArray= [];
+    if (inputString===''){
+        return score;
+    }
+    let onePoints = ['A','E','I','O','U','L','N','R','S','T'];
+    let twoPoints = ['D','G'];
+    let threePoins = ['B','C','M','P'];
+    let fourPoints = ['F','H','V','W','Y'];
+    let fivePoints = ['K'];
+    let eightPoints = ['J','X'];
+    let tenPoints = ['Q','Z'];
+
+    
+    
+     for (let i = 0; i<inputString.length;i++){
+       
+      for(let j = 0;j<onePoints.length;j++){
+        if (inputString.charAt(i)===onePoints[j]){
+          resultArray.push(1);
+          break;
+        }
+        if (inputString.charAt(i)===twoPoints[j]){
+          resultArray.push(2);
+          break;
+        }
+        if (inputString.charAt(i)===threePoins[j]){
+          resultArray.push(3);
+          break;
+        }
+        if (inputString.charAt(i)===fourPoints[j]){
+          resultArray.push(4);
+          break;
+        }
+        if (inputString.charAt(i)===fivePoints[j]){
+          resultArray.push(5);
+          break;
+        }
+        if (inputString.charAt(i)===eightPoints[j]){
+          resultArray.push(8);
+          break;
+        }
+        if (inputString.charAt(i)===tenPoints[j]){
+          resultArray.push(10);
+          break;
+        }
+      }
+     }
+
+    
+    score = resultArray.reduce((a,b) => a + b, 0);
+
+
+    return score;
+
+
+}
+
+module.exports = score;
